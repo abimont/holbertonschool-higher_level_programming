721e112a5db9b27ca1271d75530a395a527cb289
abimont <abimontes00@gmail.com>
2022-09-23 14:41:01 -0700

    lowercase to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+    counter = 0
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            x = ord(i)
+            x = x - 32
+            character = chr(x)
+            upper = upper + character
+        else:
+            upper = upper + str[counter]
+        counter = counter + 1
+    print(upper)
