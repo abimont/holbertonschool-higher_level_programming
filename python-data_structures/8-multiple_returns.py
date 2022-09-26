ccb8d199af9951a0ce0f35dbd52443064a9b053e
abimont <abimontes00@gmail.com>
2022-09-25 23:13:54 -0700

    tuple of a string

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence  == "":
+    if sentence == "":
         tuple_str = len(sentence), None
         return tuple_str
     tuple_str = len(sentence), sentence[0]
