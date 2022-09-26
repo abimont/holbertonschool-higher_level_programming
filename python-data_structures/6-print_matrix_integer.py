2830d15a70f1c296490599c3b7ac85f80cb0ab68
abimont <abimontes00@gmail.com>
2022-09-25 22:12:48 -0700

    prints a matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for number in i:
-            print(number, end=" " if number != i[-1] else "")
+            print("{:d}".format(number), end=" " if number != i[-1] else "")
         print()
