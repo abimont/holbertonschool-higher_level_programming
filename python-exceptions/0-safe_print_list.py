f99d27e6d2ab86d1daa91428c92feffd7da909ed
abimont <abimontes00@gmail.com>
2022-09-30 13:31:41 -0700

    prints x elements

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     counter = 0
-    if x == 0:
-        return 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="\n" if i == x - 1 else "")
-        except IndexError:
+        except:
             print()
             break
         counter += 1
