713908a0dd5d10a9476bd9dc9fff491e80cd83af
abimont <abimontes00@gmail.com>
2022-10-02 22:46:24 -0700

    prints a square in a position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,7 +6,7 @@ class Square:
     """ Instantiation with validatios """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
