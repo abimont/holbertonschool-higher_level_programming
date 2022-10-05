95ceaf51aeb9d23eee2a57ef29a735b02dbfdc3b
abimont <abimontes00@gmail.com>
2022-10-05 18:38:41 -0500

    pycodestyle fixed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,5 +19,5 @@ def add_integer(a, b=98):
         if type(a) is not int:
             raise TypeError("a must be an integer")
         if type(b) is not int:
-            raise TypeError("b must be an integer") 
+            raise TypeError("b must be an integer")
     return result
