03159e0ab01cd7046b5118feb96cb8896eeaa55d
abimont <abimontes00@gmail.com>
2022-10-05 23:13:06 -0500

    pycodestyle fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,7 +7,7 @@ def say_my_name(first_name, last_name=""):
     first_name and last_name must be strings
     otherwise, it raises an exception
     """
-    if type(first_name) is not str: 
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
