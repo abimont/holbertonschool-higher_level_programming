401b4508cfa2d560c91339712ede09e85b521019
abimont <abimontes00@gmail.com>
2022-10-06 00:34:39 -0500

    typo fixed

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -9,7 +9,7 @@ def print_square(size):
     """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     for row in range(size):
         for column in range(size):
