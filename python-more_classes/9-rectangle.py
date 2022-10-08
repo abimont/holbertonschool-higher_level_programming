6afa0ad93ea36b0a49d8a3853f5a015f90068ab4
abimont <abimontes00@gmail.com>
2022-10-07 20:59:33 -0500

    refrom ctangle i to square

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         type(self).number_of_instances += 1
 
     """
@@ -65,21 +65,6 @@ class Rectangle:
             else:
                 return rect_2
 
-    """ setter and getter of height property """
-    @property
-    def height(self):
-        """ Retrieves the instance's height """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Sets the height of the rectangle """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
     """ setter and getter of width property """
     @property
     def width(self):
@@ -94,6 +79,21 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    """ setter and getter of height property """
+    @property
+    def height(self):
+        """ Retrieves the instance's height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height of the rectangle """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     """
     Public instance method that returns the rectangle area
