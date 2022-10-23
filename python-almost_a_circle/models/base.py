47eb3c8543f592047cdb0ff94c6dde67d3e5e07b
abimont <abimontes00@gmail.com>
2022-10-23 18:29:41 -0500

    update: from_json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -61,3 +61,17 @@ class Base:
                 list.append(cls.to_dictionary(obj))
         with open(filename, 'w') as file:
             file.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string representation json_string
+
+        Args:
+            json_string: string representing a list of dictionaries
+        """
+
+        if json_string is None or len(json_string) <= 0:
+            return []
+        else:
+            return json.loads(json_string)
