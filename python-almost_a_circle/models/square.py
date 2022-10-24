771b457f292c450deffc2ac633e48323d659f44f
abimont <abimontes00@gmail.com>
2022-10-23 23:03:29 -0500

    update: method added

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -20,24 +20,52 @@ class Square(Rectangle):
             y: position in y
             id: instance identifier
         """
+        self.size = size
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """
-        Returns Square description
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     """ size getter and setter """
     @property
     def size(self):
-        return self.width
+        return self.__width
 
     @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-        self.width = value
-        self.height = value
+
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        """
+        Returns Square description
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__width}"
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns attributes
+
+        Args:
+            args: arguments
+        """
+        if args is not None and len(args) > 0:
+            try:
+                self.id = args[0]
+                self.__width = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                return
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.__width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
