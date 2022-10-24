a2f927e21a32e7a39feee022a85e02357cdf4925
abimont <abimontes00@gmail.com>
2022-10-23 23:07:56 -0500

    update: dictionary representation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -69,3 +69,17 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """
+        Returns the dictionary representation of a Square
+        """
+
+        dict = {
+            "id": self.id,
+            "size": self.__width,
+            "x": self.x,
+            "y": self.y
+        }
+
+        return dict
