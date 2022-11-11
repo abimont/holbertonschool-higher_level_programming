68adf2bf0691791cffbf42200a76bfc6ec9a3e02
abimont <abimontes00@gmail.com>
2022-11-11 14:54:48 -0500

    File  fixed

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in the MySQL server.
 -- results are displayed both the score and the name (in this order) ordered by score (top first)
-SELECT score, name FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC;
