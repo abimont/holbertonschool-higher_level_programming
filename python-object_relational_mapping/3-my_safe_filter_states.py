76928e16f46e3f5a8a725d3c90afdfbaf36a2cb8
abimont <abimontes00@gmail.com>
2022-11-17 23:57:09 -0500

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], database=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (argv[4],))
+    cur.execute(
+        """SELECT * FROM states
+        WHERE name LIKE BINARY %s
+        ORDER BY id ASC""", (argv[4],))
 
     query_rows = cur.fetchall()
     for row in query_rows:
