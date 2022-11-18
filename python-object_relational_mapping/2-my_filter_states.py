462c6533099b7e816bd0b18b2e225c6299149ac6
abimont <abimontes00@gmail.com>
2022-11-17 20:27:18 -0500

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
                            user=argv[1], passwd=argv[2], database=argv[3])
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4]))
+        """SELECT * FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY id ASC""".format(argv[4]))
 
     query_rows = cur.fetchall()
     for row in query_rows:
