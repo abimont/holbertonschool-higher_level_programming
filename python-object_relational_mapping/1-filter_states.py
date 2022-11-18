ce0b4c5dfdfe48e0dc0741004b5eacac44b1910d
abimont <abimontes00@gmail.com>
2022-11-17 20:17:49 -0500

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ from sys import argv
 import MySQLdb
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], database=argv[3])
     cur = conn.cursor()
