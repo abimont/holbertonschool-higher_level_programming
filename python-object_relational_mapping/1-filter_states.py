97b7219bcbe024511fcbc4e8a41c1365dff31f3a
abimont <abimontes00@gmail.com>
2022-11-17 20:16:15 -0500

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,6 +3,9 @@
 script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
+from sys import argv
+import MySQLdb
+
 
 if __name__ = "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
