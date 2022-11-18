42c34a21fa2ec6da00baf67cc031bba44a3110a6
abimont <abimontes00@gmail.com>
2022-11-18 00:03:32 -0500

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         """SELECT * FROM states
-        WHERE name LIKE BINARY '%s'
-        ORDER BY id ASC""", (argv[4],))
+        WHERE name LIKE BINARY '%s'""", (argv[4],))
 
     query_rows = cur.fetchall()
     for row in query_rows:
