f36c612699c01a75d9afafe7764d7c9451562569
abimont <abimontes00@gmail.com>
2022-11-17 23:43:11 -0500

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa
+script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa
 """
 from sys import argv
 import MySQLdb
@@ -11,9 +12,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], database=argv[3])
     cur = conn.cursor()
-    cur.execute(
-        """SELECT * FROM states
-        WHERE name = %s""", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
 
     query_rows = cur.fetchall()
     for row in query_rows:
