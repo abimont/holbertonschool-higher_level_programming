207dbb4749b6eb4ccf1d67e2e3746832667cb1f8
abimont <abimontes00@gmail.com>
2022-12-19 15:59:23 -0800

    Arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const process = require('process');
 const args = process.argv;
 
 if (args.length < 3) {
-	  console.log('No argument');
+  console.log('No argument');
 } else if (args.length === 3) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }
