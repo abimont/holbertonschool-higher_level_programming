133a46d6333616c4ee5e3e80db695f2e9d712a71
abimont <abimontes00@gmail.com>
2022-12-20 10:54:22 -0500

    An Integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+if (!args[2]) {
+  console.log('Not a number');
+} else if (isNaN(args[2])) {
+  console.log('Not a number');
+} else {
+  console.log(parseInt(args[2]));
+}
