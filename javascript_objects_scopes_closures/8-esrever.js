77be766237aa0a846288e0bc1717ebbcb32365f7
abimont <abimontes00@gmail.com>
2022-12-21 16:33:58 -0500

    Esrever

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  var reversed = Array();
+  const reversed = Array();
   for (let i = list.length - 1; i >= 0; i--) {
     reversed.push(list[i]);
   }
