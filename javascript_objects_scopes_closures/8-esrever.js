07dcfb5ced41a784b7a2002f636bac7564f1a19b
abimont <abimontes00@gmail.com>
2022-12-21 16:35:06 -0500

    Semistandard validation

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const reversed = Array();
+  const reversed = [];
   for (let i = list.length - 1; i >= 0; i--) {
     reversed.push(list[i]);
   }
