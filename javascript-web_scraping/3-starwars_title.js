1742609c9898742aa83800d0c06ba00032d60043
abimont <abimontes00@gmail.com>
2022-12-22 21:35:47 -0500

    Star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const args = process.argv;
 const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
 
-request(url, function callback (error, response, body) {
+request(url, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const result = JSON.parse(body);
     console.log(result.title);
